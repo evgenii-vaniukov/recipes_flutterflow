9d077acae15708eba4c3c36f74fec6be3d69aee5
Evgenii Vaniukov <evgeniivaniukov64@gmail.com>
2023-03-19 17:11:25 +0200

    delete

--- lib/index.dart
+++
@@ -1,9 +0,0 @@
-// Export pages
-export '/screens/search_recipes/search_recipes_widget.dart'
-    show SearchRecipesWidget;
-export '/screens/recipe_details_a_p_i/recipe_details_a_p_i_widget.dart'
-    show RecipeDetailsAPIWidget;
-export '/screens/bookmarks/bookmarks_widget.dart' show BookmarksWidget;
-export '/screens/explore/explore_widget.dart' show ExploreWidget;
-export '/screens/recipe_details_firebase/recipe_details_firebase_widget.dart'
-    show RecipeDetailsFirebaseWidget;
